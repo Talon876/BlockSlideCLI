6b4d0046f7b48d6b5ae0108c580557ff67aabd8a
Talon Daniels <talondaniels@gmail.com>
2015-03-10 16:39:59 -0600

    Update AssemblyInfo

--- BlockSlideCLI/Properties/AssemblyInfo.cs
+++ BlockSlideCLI/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BlockSlideCLI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Command line implementation of BlockSlide.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Talon Daniels")]
 [assembly: AssemblyProduct("BlockSlideCLI")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
