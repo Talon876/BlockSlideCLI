ebbd43d3cd5368869bb2e086d25cad4411a03748
Talon Daniels <talondaniels@gmail.com>
2015-03-15 15:42:07 -0600

    Add ToCharacter utility method to TileType

--- BlockSlideCore/Entities/TileType.cs
+++ BlockSlideCore/Entities/TileType.cs
@@ -1,4 +1,6 @@
-﻿namespace BlockSlideCore.Entities
+﻿using System.Collections.Generic;
+
+namespace BlockSlideCore.Entities
 {
     public enum TileType
     {
@@ -6,5 +8,20 @@
         Wall = 1,
         Start = 2,
         Finish = 3,
+    }
+
+    public static class TileTypeUtils
+    {
+        public static char ToCharacter(this TileType value)
+        {
+            var mapping = new Dictionary<TileType, char>
+            {
+                {TileType.Floor, '.'},
+                {TileType.Wall, '#'},
+                {TileType.Start, '@'},
+                {TileType.Finish, '$'},
+            };
+            return mapping[value];
+        }
     }
 }
