cc948cfdc0f7c62d7c8085c7988360011f54f2e4
Talon Daniels <talondaniels@gmail.com>
2015-03-15 19:14:20 -0600

    Remove unused GenerateDotFiles method in main

--- BlockSlideCLI/Program.cs
+++ BlockSlideCLI/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using BlockSlideCore.Analysis;
-using BlockSlideCore.Entities;
-using BlockSlideCore.Levels;
 
 namespace BlockSlideCLI
 {
@@ -12,23 +9,8 @@ namespace BlockSlideCLI
             Console.WriteLine("Welcome to BlockSlide!");
             var blockslide = new BlockSlideCLI();
             blockslide.Start();
-
             Console.Read();
         }
 
-        public static void GenerateDotFiles()
-        {
-            const string DIRECTORY = "RandomSmallDotFiles";
-            var dotFileGenerator = new DotFileGenerator();
-            const int COUNT = 50;
-            for (var i = 1; i <= COUNT; i++)
-            {
-                var level = new Level(i, new RandomLevelBuilder(Config.WIDTH, Config.HEIGHT));
-                dotFileGenerator.GenerateDotFile(level, DIRECTORY);
-            }
-            dotFileGenerator.GenerateBatchConvertFile(DIRECTORY,
-                @"C:\Users\Talon\SkyDrive\bin\apps\graphviz-2.38\release\bin\", COUNT);
-
-        }
     }
 }
