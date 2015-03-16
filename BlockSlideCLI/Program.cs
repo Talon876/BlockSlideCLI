dc32f4d4ea9856dabd2a4d26dd59cdbca2a562f9
Talon Daniels <talondaniels@gmail.com>
2015-03-16 05:44:01 -0600

    Remove favicon generation

--- BlockSlideCLI/Program.cs
+++ BlockSlideCLI/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Drawing.Imaging;
-using BlockSlideCore.Analysis;
-using BlockSlideCore.Entities;
-using BlockSlideCore.Levels;
 
 namespace BlockSlideCLI
 {
@@ -11,13 +7,7 @@ namespace BlockSlideCLI
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to BlockSlide!");
-
-            var randomLevel = new Level(1, new RandomLevelBuilder(8, 8));
-            var imageBuilder = new LevelImageGenerator();
-            var image = imageBuilder.GenerateImage(randomLevel, 16);
-            image.Save("favicon.png", ImageFormat.Png);
-
-
+            
             var blockslide = new BlockSlideCLI();
             blockslide.Start();
             Console.Read();
